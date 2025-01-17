51a3734af9da4f999deb3fe475a6d39325e50941
luktar <luktar@gmail.com>
2025-01-17 19:46:34 +0100

    Removed old program1

--- Program1/program1_b.py
+++
@@ -1,7 +0,0 @@
-'''
-Napisz program, który wypisze frazę "Hello World!", ale zastosuj do tego
-łączenia stringów poprzez symbol "+".
-'''
-
-print("Hello" + " " + "World!")
-print("H" + "e" + "llo Wor" + "ld!")
